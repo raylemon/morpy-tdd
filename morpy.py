86d724a781046c7b276ff305c3f9fb1faa9dde64
Ray Lemon <big0424@gmail.com>
2021-02-25 18:22:59 +0100

    Premier test : succès !

--- morpy.py
+++ morpy.py
@@ -1,8 +1,10 @@
 # Définition des variables
-play_board = [["" for _ in range(3)] for _ in range(3)] # le tableau de jeu
-SYMBOLS = ("X", "O") # les symboles joués
-computer_is_playing = True # l’ordinateur joue en premier
+import random
+
+play_board = [["" for _ in range(3)] for _ in range(3)]  # le tableau de jeu
+SYMBOLS = ("X", "O")  # les symboles joués
+computer_is_playing = True  # l’ordinateur joue en premier
 
 
-def play()->int:
-    pass
+def play() -> int:
+    return random.randint(1, 9)

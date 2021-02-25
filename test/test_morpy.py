de94092d5f11b398689d903d3aead5e4fe7a950a
Ray Lemon <big0424@gmail.com>
2021-02-25 22:13:40 +0100

    Commit erroné - Les tests vont échouer. Ce commit montre l’intérêt de la fixture

--- test/test_morpy.py
+++ test/test_morpy.py
@@ -2,20 +2,20 @@ import morpy
 import pytest
 
 
-@pytest.fixture
-def setup_any_board():
-    tmp = morpy.play_board.copy()
-    morpy.play_board = [["", "O", "X"],["X", "", "O"],["O", "X",""]]
-    yield
-    morpy.play_board = tmp\
+# @pytest.fixture
+# def setup_any_board():
+#    tmp = morpy.play_board.copy()
+#    morpy.play_board = [["", "O", "X"],["X", "", "O"],["O", "X",""]]
+#    yield
+#    morpy.play_board = tmp
 
 
-@pytest.fixture
-def setup_other_board():
-    tmp = morpy.play_board.copy()
-    morpy.play_board = [["X", "O", ""],["X", "O", "O"],["O", "X","X"]]
-    yield
-    morpy.play_board = tmp
+# @pytest.fixture
+# def setup_other_board():
+#    tmp = morpy.play_board.copy()
+#    morpy.play_board = [["X", "O", ""],["X", "O", "O"],["O", "X","X"]]
+#    yield
+#    morpy.play_board = tmp
 
 
 def test_computer_plays():
@@ -23,10 +23,22 @@ def test_computer_plays():
     assert 1 <= morpy.play() <= 9
 
 
-def test_computer_plays_any_board(setup_any_board):
-    """Doit retourner un chiffre représentant une case vide"""
-    assert morpy.play() in [3,6,9]
+# def test_computer_plays_any_board(setup_any_board):
+#    """Doit retourner un chiffre représentant une case vide"""
+#    assert morpy.play() in [3,6,9]
 
-def test_computer_plays_other_board(setup_other_board):
+# def test_computer_plays_other_board(setup_other_board):
+#    """Doit retourner un chiffre représentant une case vide"""
+#    assert morpy.play() == 3
+
+
+def test_computer_plays_any_board():
     """Doit retourner un chiffre représentant une case vide"""
+    morpy.play_board = [["", "O", "X"], ["X", "", "O"], ["O", "X", ""]]
+    assert morpy.play() in [3, 6, 9]
+
+
+def test_computer_plays_other_board():
+    """Doit retourner un chiffre représentant une case vide"""
+    morpy.play_board = [["X", "O", ""], ["X", "O", "O"], ["O", "X", "X"]]
     assert morpy.play() == 3

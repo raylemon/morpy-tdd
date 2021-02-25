245c1283d46112f156750308c45ae6d32f701f2f
Ray Lemon <big0424@gmail.com>
2021-02-25 19:01:40 +0100

    Second test : la fixture

--- test/test_morpy.py
+++ test/test_morpy.py
@@ -1,6 +1,20 @@
 import morpy
+import pytest
+
+
+@pytest.fixture
+def setup_any_board():
+    tmp = morpy.play_board.copy()
+    morpy.play_board = [["X", "O", ""],["X", "O", ""],["O", "X",""]]
+    yield tmp
+    morpy.play_board = tmp
 
 
 def test_computer_plays():
     """ Doit retourner un chiffre entre 1 et 9"""
     assert 1 <= morpy.play() <= 9
+
+
+def test_computer_plays_any_board(setup_any_board):
+    """Doit retourner un chiffre représentant une case vide"""
+    assert morpy.play() in [3,6,9]

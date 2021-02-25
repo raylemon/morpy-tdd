815252b2e1a74b5599899c4d5e2165cc333a8509
Ray Lemon <big0424@gmail.com>
2021-02-25 22:58:20 +0100

    Afficher deux configurations de tableaux différentes. Notez la duplication de code, et la réutilisation des fixtures. On va modifier les tests plus tard.

--- test/test_morpy.py
+++ test/test_morpy.py
@@ -37,3 +37,23 @@ def test_show_board(capsys):
     """"Affiche le tableau de jeu vide"""
     morpy.show_board()
     assert "1 2 3 \n4 5 6 \n7 8 9 \n" == capsys.readouterr().out
+
+
+def test_show_board_any_board(setup_any_board,capsys):
+    """Affiche le tableau de jeu
+        X O 3
+        X O 6
+        O X 9
+    """
+    morpy.show_board()
+    assert "X O 3 \nX O 6 \nO X 9 \n" == capsys.readouterr().out
+
+
+def test_show_board_other_board(setup_other_board,capsys):
+    """Affiche le tableau de jeu
+        X O 3
+        X O O
+        O X X
+    """
+    morpy.show_board()
+    assert "X O 3 \nX O O \nO X X \n" == capsys.readouterr().out

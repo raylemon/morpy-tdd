a0fc8a48ee432b8b7339cb58b6246abab2bbe803
Ray Lemon <big0424@gmail.com>
2021-02-25 18:06:44 +0100

    Définitions des variables utiles au jeu (Peut être modifié à l’avenir)

---
+++ src/morpy.py
@@ -0,0 +1,4 @@
+# Définition des variables
+play_board = [["" for _ in range(3)] for _ in range(3)] # le tableau de jeu
+SYMBOLS = ("X", "O") # les symboles joués
+computer_is_playing = True # l’ordinateur joue en premier

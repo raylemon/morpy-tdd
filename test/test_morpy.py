38fceacf0c75970ef111d136e97e69f972997d15
Ray Lemon <big0424@gmail.com>
2021-02-25 22:00:07 +0100

    Troisième test : succès ! (malgré une duplication de code…) sans changer morpy !

--- test/test_morpy.py
+++ test/test_morpy.py
@@ -5,8 +5,16 @@ import pytest
 @pytest.fixture
 def setup_any_board():
     tmp = morpy.play_board.copy()
-    morpy.play_board = [["X", "O", ""],["X", "O", ""],["O", "X",""]]
-    yield tmp
+    morpy.play_board = [["", "O", "X"],["X", "", "O"],["O", "X",""]]
+    yield
+    morpy.play_board = tmp\
+
+
+@pytest.fixture
+def setup_other_board():
+    tmp = morpy.play_board.copy()
+    morpy.play_board = [["X", "O", ""],["X", "O", "O"],["O", "X","X"]]
+    yield
     morpy.play_board = tmp
 
 
@@ -18,3 +26,7 @@ def test_computer_plays():
 def test_computer_plays_any_board(setup_any_board):
     """Doit retourner un chiffre représentant une case vide"""
     assert morpy.play() in [3,6,9]
+
+def test_computer_plays_other_board(setup_other_board):
+    """Doit retourner un chiffre représentant une case vide"""
+    assert morpy.play() == 3

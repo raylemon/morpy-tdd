8bc77cdc487486616d4e5329c975e300689f7baa
Ray Lemon <big0424@gmail.com>
2021-02-27 14:01:51 +0100

    Écriture du main, rassemblement des fonctions.
    Le jeu est fonctionnel !

--- morpy.py
+++ morpy.py
@@ -2,7 +2,7 @@
 import random
 
 play_board = [["" for _ in range(3)] for _ in range(3)]  # le tableau de jeu
-computer_is_playing = True  # l’ordinateur joue en premier
+human_is_playing = False  # l’ordinateur joue en premier
 
 
 def play(symbol, is_human: bool = False) -> int:
@@ -111,3 +111,43 @@ def update_board(symbol: str, cell: int):
 def clear_board():
     for i in range(9):
         play_board[i // 3][i % 3] = ""
+
+
+if __name__ == '__main__':
+    replay = True
+    while replay:
+        player_symbol = ""
+        while player_symbol not in ["O","X"]:
+            player_symbol = input("Joueur, entrez votre symbole de jeu [O,X]:").upper()
+        print(f"Joueur, vous jouez avec les {player_symbol}\nVotre adversaire commence")
+
+        active_symbol = "O" if player_symbol == "X" else "X"
+        end_game = False
+        while not end_game:
+            cell = play(active_symbol, human_is_playing)
+            print("Joueur" if human_is_playing else "Ordinateur", f"joue en {cell} !,\n")
+            update_board(active_symbol,cell)
+            #2
+            if is_won(active_symbol):
+                print("Joueur" if human_is_playing else "Ordinateur", "à gagné !!!\n")
+                end_game = True
+            elif cell == -1:
+                print("Égalité…")
+                end_game = True
+            show_board() #2
+
+            human_is_playing = not human_is_playing
+            active_symbol = "O" if active_symbol == "X" else "X"
+
+        rep = ""
+        while rep not in ["O", "N"]:
+            rep = input("Souhaitez-vous rejouer ? [O/N]:").upper()
+
+        if rep == "O":
+            clear_board()
+            human_is_playing = False
+            replay = True
+        else:
+            replay = False
+    else:
+        print("Merci d’avoir joué !!")
